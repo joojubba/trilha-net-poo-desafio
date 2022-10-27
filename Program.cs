ca1701736f16a8c49ab8beeca1465c2ce05f0009
Joanna Lima <joanna-karla@hotmail.com>
2022-10-27 08:40:30 -0300

    Desafio Realizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using static System.Console;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+WriteLine("## Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+WriteLine("\n");
+
+WriteLine("## Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "87654321", modelo: "Modelo 2", imei: "222222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
